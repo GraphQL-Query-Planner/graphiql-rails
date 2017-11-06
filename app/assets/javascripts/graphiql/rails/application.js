236e628eab6fa8cd39629afe751307c0dc47335d
Ismail Syed <ismail.syed@shopify.com>
2017-11-06 16:20:50 -0500

    Updates assets with GraphQL-Query-Planner/graphiql updates

--- app/assets/javascripts/graphiql/rails/application.js
+++ app/assets/javascripts/graphiql/rails/application.js
@@ -1,4 +1,4 @@
-//= require ./react-15.6.2
-//= require ./react-dom-15.6.2
+//= require ./react-15.5.4
+//= require ./react-dom-15.5.4
 //= require ./fetch-0.10.1
-//= require ./graphiql-0.11.5
+//= require ./graphiql-0.11.10
